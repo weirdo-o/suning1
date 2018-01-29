76492a95e86dadea8380ccaaf2caea5951980d10
weirdo-o <35020904+weirdo-o@users.noreply.github.com>
2018-01-29 22:40:14 +0800

    Update sn.js

--- sn.js
+++ sn.js
@@ -5,6 +5,9 @@ window.onload=function () {
         let n=0;
         let lis=document.querySelectorAll('.bannerpic li');
         let banner=document.querySelector('.bannerpic')
+        let right=document.querySelector('.jiantoudi.right')
+        let left=document.querySelector('.jiantoudi.left')
+        let cirs=document.querySelectorAll('.lunbodian li')
         function move() {
             n++;
             if (n>=lis.length) {
@@ -12,8 +15,10 @@ window.onload=function () {
             }
             lis.forEach(function (dom, index) {
                 dom.classList.remove('active');
+                cirs[index].classList.remove('active')
             })
             lis[n].classList.add('active')
+            cirs[n].classList.add('active')
 
         }
         let t=setInterval(move,2000)
@@ -23,19 +28,53 @@ window.onload=function () {
         banner.onmouseout=function () {
             t=setInterval(move,2000)
         }
-        document.querySelector('.jiantoudi.right').onclick=function () {
+        let flag=true
+        right.onclick=function () {
+            if(!flag){
+                return
+            }
+            flag=false
             move()
         }
-        document.querySelector('.jiantoudi.left').onclick=function () {
+        left.onclick=function () {
+            if(!flag){
+                return
+            }
+            flag=false
             n--;
             if(n<0){
                 n=lis.length-1
             }
             lis.forEach(function (dom, index) {
                 dom.classList.remove('active');
+                cirs[index].classList.remove('active')
             })
             lis[n].classList.add('active')
+            cirs[n].classList.add('active')
         }
+
+    cirs.forEach(function (val,index) {
+        val.onclick=function () {
+            if(!flag){
+                return
+            }
+            flag=false
+            lis.forEach(function (va,ind) {
+                va.classList.remove('active')
+                cirs[ind].classList.remove('active')
+            })
+            lis[index].classList.add('active')
+            this.classList.add('active')
+            n=index
+        }
+    })
+
+    lis.forEach(function (dom) {
+        dom.addEventListener('transitionend',function () {
+            flag=true
+        })
+    })
+
 
     let nav=document.querySelectorAll('.djh-top .dajuhui_top_center');
     let list=document.querySelectorAll('.dajuhui .djh-bottom')
